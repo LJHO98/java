f7e6a2d408210e7354dd329c9346726636bb5239
LJHO98 <ehdgoanf714@naver.com>
2024-07-23 09:55:58 +0900

    123

--- calculator/src/caculator/DivCal.java
+++ calculator/src/caculator/DivCal.java
@@ -4,7 +4,7 @@ public class DivCal extends Caculator{ //나누기
 	@Override
 	protected double cal(double num1, double num2) {
 		if(num1==0) {
-			System.out.println("ㅇㅇㅇ");
+			System.out.println("잘못된숫자입력");
 			return 0;
 		}
 		return num1/num2;
