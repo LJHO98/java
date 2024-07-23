fa302a64c35df104aecf719d78930450f1ad650a
LJHO98 <78121116+LJHO98@users.noreply.github.com>
2024-07-23 11:22:00 +0900

    Update MinusCal.java

--- calculator/src/caculator/MinusCal.java
+++ calculator/src/caculator/MinusCal.java
@@ -1,6 +1,6 @@
 package caculator;
 
-public class MinusCal extends Caculator{ //»©±â
+public class MinusCal extends Caculator{ //ë¹¼ê¸°
 	@Override
 	protected double cal(double num1, double num2) {
 		return num1-num2;
